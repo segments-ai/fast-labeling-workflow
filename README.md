4963a462b991fe2763892f6510cdafc77179e3df
Bert De Brabandere <db.bert@gmail.com>
2020-07-31 16:51:36 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/segments-ai/predictive-labeling-workflow/blob/master/demo.ipynb)
+
+# Speeding up your labeling workflow for image segmentation
+
+A large dataset of labeled images is the first thing you need in any serious computer vision project.
+Building such datasets is a time-consuming endeavour, involving lots of manual labeling work. This is especially true for tasks like image segmentation where the labels need to be very precise.
+
+One way to drastically speed up image labeling is by leveraging your machine learning models from the start. 
+Instead of labeling the entire dataset manually, you can use your model to help you by iterating between image labeling and model training.
+
+This tutorial will show you how to achieve such a fast labeling workflow for image segmentation with Segments.ai.
+
+![A fast labeling workflow](img/fast-labeling-workflow-diagram.png)
+
+[Segments.ai](https://segments.ai) is a labeling platform with powerful automation tools for image segmentation. 
+It also features a flexible API and Python SDK, which enable you to quickly set up custom workflows by uploading images and labels directly from your code.
+
+We will walk you through a simple but efficient setup:
+
+1. Upload your images to Segments.ai, and label a small subset.
+2. Train a segmentation model on the labeled images.
+3. Generate label predictions on the remaining images and upload them.
+4. Correct the mistakes.
+
+You can find all code for this tutorial on [Github](https://github.com/segments-ai/fast-labeling-workflow), or follow along on [Google Colab](https://colab.research.google.com/github/segments-ai/fast-labeling-workflow/blob/master/demo.ipynb).
---
+++ utils.py
@@ -0,0 +1,104 @@
+# Do some pip install stuff here in colab
+
+import os
+import json
+import urllib.request
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultTrainer, DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets import register_coco_instances, load_coco_json
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pycocotools import mask
+
+from segments.utils import export_dataset
+
+
+class Model:
+    def __init__(self, predictor):
+        self.predictor = predictor
+
+    def _convert_to_segments_format(self, image, outputs):
+        # https://tech.amikelive.com/node-718/what-object-categories-labels-are-in-coco-dataset/
+        segmentation_bitmap = np.zeros((image.shape[0], image.shape[1]), np.uint32)
+        annotations = []
+        counter = 1
+        instances = outputs['instances']
+        for i in range(len(instances.pred_classes)):
+            category_id = int(instances.pred_classes[i])
+            instance_id = counter
+            mask = instances.pred_masks[i].cpu()
+            segmentation_bitmap[mask] = instance_id
+            annotations.append({'id': instance_id, 'category_id': category_id})
+            counter += 1
+        return segmentation_bitmap, annotations
+
+    def __call__(self, image):
+        image = np.array(image)
+        outputs = self.predictor(image)
+        label, label_data = self._convert_to_segments_format(image, outputs)
+
+        return label, label_data
+    
+
+def train_model(dataset):
+    # Export the dataset to COCO format
+    export_file, image_dir = export_dataset(dataset)
+    
+    # Register it as a COCO dataset in the Detectron2 framework
+    try:
+        register_coco_instances('my_dataset', {}, export_file, image_dir)
+    except:
+        print('Dataset was already registered')
+    dataset_dicts = load_coco_json(export_file, image_dir)
+    MetadataCatalog.get('my_dataset').set(thing_classes=[c['name'] for c in dataset.categories])
+    segments_metadata = MetadataCatalog.get('my_dataset')
+    print(segments_metadata)
+    
+    # Configure the training run
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'))
+    cfg.DATASETS.TRAIN = ('my_dataset',)
+    cfg.DATASETS.TEST = ()
+    cfg.INPUT.MASK_FORMAT = 'bitmask'
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml')  # Let training initialize from model zoo
+    cfg.SOLVER.IMS_PER_BATCH = 2 # 4
+    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(dataset.categories)  # number of categories
+#     cfg.MODEL.DEVICE = 'cuda'
+
+    # Start the training
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg) 
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+    
+    # Return the model
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, 'model_final.pth')
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
+    cfg.DATASETS.TEST = ('my_dataset', )
+    predictor = DefaultPredictor(cfg)
+    model = Model(predictor)
+
+    return model
+    
+    
+def get_image_urls(topic):
+    with open('{}.json'.format(topic)) as json_file:
+        image_urls = json.load(json_file)
+    return image_urls
+
+
+def visualize(*args):
+    images = args
+    for i, image in enumerate(images):
+        plt.subplot(1,len(images),i+1)
+        plt.imshow(np.array(image))
+    plt.show()
